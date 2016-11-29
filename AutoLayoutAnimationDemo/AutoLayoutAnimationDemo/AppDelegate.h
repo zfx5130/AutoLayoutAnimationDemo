51ff4015c64aedfddb69ca1fdf9027f4da9aad25
thomas <dui1cuo@126.com>
2016-11-29 13:46:37 +0800

    [Mod]minor fix

---
+++ AutoLayoutAnimationDemo/AutoLayoutAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutAnimationDemo
+//
+//  Created by admin on 16/11/29.
+//  Copyright © 2016年 thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutAnimationDemo/AutoLayoutAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutAnimationDemo
+//
+//  Created by admin on 16/11/29.
+//  Copyright © 2016年 thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
